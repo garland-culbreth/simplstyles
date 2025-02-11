71c204b1e76c46f86d0eb52fe151c9fe8de1f2c7
Garland Culbreth <culbreth.garland@gmail.com>
2025-02-11 00:17:03 -0800

    fix export syntax

--- simplstyles/__init__.py
+++ simplstyles/__init__.py
@@ -1,7 +1,9 @@
 import os
 import matplotlib.pyplot as plt
 
+styles = {}
 for dir, _, _ in os.walk("simplstyles"):
-    styles = plt.style.core.read_style_directory(dir)
-    plt.style.core.update_nested_dict(plt.style.library, styles)
+    new_styles = plt.style.core.read_style_directory(dir)
+    styles.update(new_styles)
+plt.style.core.update_nested_dict(plt.style.library, styles)
 plt.style.core.available[:] = sorted(plt.style.library.keys())
