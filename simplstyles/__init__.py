63f21ab371ec54b0bd0ede86ab8c3ff55d55983f
Garland Culbreth <culbreth.garland@gmail.com>
2025-02-10 23:51:32 -0800

    remove debug command

--- simplstyles/__init__.py
+++ simplstyles/__init__.py
@@ -5,4 +5,3 @@ for dir, _, _ in os.walk("simplstyles"):
     styles = plt.style.core.read_style_directory(dir)
     plt.style.core.update_nested_dict(plt.style.library, styles)
 plt.style.core.available[:] = sorted(plt.style.library.keys())
-print(plt.style.available)
