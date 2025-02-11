5b8e12df55b20ae77bd4005415ec4e6eb3428f23
Garland Culbreth <culbreth.garland@gmail.com>
2025-02-10 23:42:07 -0800

    fix style module importation

---
+++ simplstyles/__init__.py
@@ -0,0 +1,8 @@
+import os
+import matplotlib.pyplot as plt
+
+for dir, _, _ in os.walk("simplstyles"):
+    styles = plt.style.core.read_style_directory(dir)
+    plt.style.core.update_nested_dict(plt.style.library, styles)
+plt.style.core.available[:] = sorted(plt.style.library.keys())
+print(plt.style.available)
